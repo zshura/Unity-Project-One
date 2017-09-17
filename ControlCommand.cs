1b9c3a02ee436107953be2af173ec219495d5b04
zshura <31923653+zshura@users.noreply.github.com>
2017-09-17 17:26:08 -0400

    Add files via upload

---
+++ ControlCommand.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlCommand : MonoBehaviour {
+
+	float time;
+	int myScore;
+	bool printedMessage;
+
+	// Use this for initialization
+	void Start () {
+		print ("the game has begun!");
+
+		printedMessage = false;
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+
+		if (Time.time > 3 && printedMessage == false){
+			print("it has been three seconds!");
+			printedMessage = true;
+		}
+	}
+}
